cce37cd48aa1c099d7568e1fe850d42e7614d423
Nam3001 <nguyenvn30012003@gmail.com>
2023-02-01 19:48:47 +0700

    init project

---
+++ java_basic/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_basic;
+
+/**
+ *
+ * @author nam_nguyen
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
